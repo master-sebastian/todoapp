b18a585e802c5fbc9c50e078218bbb8db56c8333
Sebastian G <orjuela.desarrollador@gmail.com>
2024-07-16 18:36:20 -0500

    Usando variables y la directiva de control *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common' //Agregarlo para versiones recientes para usar las directivas de control antiguas como el *ngFor
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule], //no olvidar incluir el CommonModule en el arreglo de imports
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todoapp';
+  title = ' Estamos probando';
+  programmingLanguages = [
+    "Java",
+    "Python",
+    "PHP",
+    "Javascript"
+  ];
 }
