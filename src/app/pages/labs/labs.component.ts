0cf5949a6435e3b2520dd50b111b3039fc56adb2
Sebastian G <orjuela.desarrollador@gmail.com>
2024-07-16 19:05:28 -0500

    String interpolation and  Property Binding

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common' //Agregarlo para versiones recientes para usar las directivas de control antiguas como el *ngFor
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
+  title = ' Estamos probando';
+  programmingLanguages = [
+    "Java",
+    "Python",
+    "PHP",
+    "Javascript"
+  ];
 
+  nameCourse = "";
+
+  private ID = 31233; //Propiedades privadas solo accesibles desde la clase
 }
